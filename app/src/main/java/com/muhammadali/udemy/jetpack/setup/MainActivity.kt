3859fd8c2afc56d1067ea37d6d26ced9a10e0cfd
Ali <muhammad.ali9385@gmail.com>
2020-04-27 04:51:15 +0500

    Initial commit

---
+++ app/src/main/java/com/muhammadali/udemy/jetpack/setup/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.muhammadali.udemy.jetpack.setup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+/**
+ * Created by Muhammad Ali on 26-Apr-20.
+ * Email muhammad.ali9385@gmail.com
+ */
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
