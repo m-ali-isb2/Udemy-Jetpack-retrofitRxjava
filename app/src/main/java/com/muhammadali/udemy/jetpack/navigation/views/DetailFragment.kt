ffaf568545a9162a4fb4cc6eee1fc8b9aee3f5f8
Ali <muhammad.ali9385@gmail.com>
2020-04-27 08:32:30 +0500

    initial commit

---
+++ app/src/main/java/com/muhammadali/udemy/jetpack/navigation/views/DetailFragment.kt
@@ -0,0 +1,47 @@
+package com.muhammadali.udemy.jetpack.navigation.views
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
+import com.muhammadali.udemy.jetpack.navigation.R
+import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.fragment_list.*
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class DetailFragment : Fragment() {
+    var dogUuid = 0
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //retrieving Argument
+        arguments?.let {
+            dogUuid = DetailFragmentArgs.fromBundle(it).dogUuid
+            detailText.text = dogUuid.toString()
+
+        }
+
+        buttonList.setOnClickListener {
+            val action: NavDirections =
+                DetailFragmentDirections.actionDetailFragmentToListFragment()
+            Navigation.findNavController(it).navigate(action)
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/muhammadali/udemy/jetpack/navigation/views/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.muhammadali.udemy.jetpack.navigation.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.muhammadali.udemy.jetpack.navigation.R
+
+
+/**
+ * Created by Muhammad Ali on 26-Apr-20.
+ * Email muhammad.ali9385@gmail.com
+ */
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
+    }
+}
